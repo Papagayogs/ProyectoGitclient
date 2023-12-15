6d032f1eb89797b568fc68ecb81a7c90b6e158b2
Papagayogs <eduardodelafuente06@gmail.com>
2023-12-15 10:38:18 +0100

    Archivo GitClient
    Creamos un archivo llamado GitClient.java

---
+++ AppGitClient/src/appgitclient/GitClient.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appgitclient;
+
+/**
+ *
+ * @author fuehered
+ */
+public class GitClient {
+
+    private String cliente;
+    private String version;
+    private String sistemaOper;
+    private String licencia;
+
+    public GitClient() {
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getSistemaOper() {
+        return sistemaOper;
+    }
+
+    public void setSistemaOper(String sistemaOper) {
+        this.sistemaOper = sistemaOper;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+}
